da2d677745f5c0693e041e4b0b7b6fac6da059b0
Isabell Long <isabell@issyl0.co.uk>
2017-07-12 11:19:41 +0100

    Handle command line arguments - print a usage message if either is nil

--- lib/vcloud/converter.rb
+++ lib/vcloud/converter.rb
@@ -1,7 +1,25 @@
-require "vcloud/converter/version"
+require_relative "converter/version"
+require 'optparse'
 
 module Vcloud
   module Converter
-    # Your code goes here...
+    ARGV << '-h' if ARGV.empty?
+    @options = {}
+    OptionParser.new do |opts|
+      opts.banner = "Usage: ruby converter.rb -t vcloud-net_launcher -p path/to/file.yaml"
+
+      opts.on('-t', '--type TYPE', 'Type of YAML file for conversion.') { |v| @options[:type] = v }
+      opts.on('-p', '--path PATH', 'Path to YAML file for conversion.') { |v| @options[:path] = v }
+
+      opts.on_tail("-h", "--help", "Show this help message.") do
+        puts opts
+        exit
+      end
+    end.parse!
+
+    def self.convert
+    end
   end
+
+  Vcloud::Converter.convert
 end
