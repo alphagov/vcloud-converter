47a9966a7295cfd27f1a27859d0f3cc0dc72418e
Isabell Long <isabell@issyl0.co.uk>
2017-08-02 10:17:50 +0100

    Error if the file is invalid YAML

--- lib/vcloud/converter.rb
+++ lib/vcloud/converter.rb
@@ -11,22 +11,28 @@ module Vcloud
     end
 
     def self.convert_vcloud_net_launcher(file_path)
-      vcloud_config = YAML.load_file(file_path)
-      name = vcloud_config.first.first
+      begin
+        vcloud_config = YAML.load_file(file_path)
+      rescue Psych::SyntaxError
+        puts "Your YAML file had a syntax error!"
+        exit
+      else
+        name = vcloud_config.first.first
 
-      vcloud_config[name].each do |machine|
-        vdc_name = machine["vdc_name"]
-        edge_gateway = machine["edge_gateway"]
-        gateway = machine["gateway"]
-        netmask = machine["netmask"]
-        dns1 = machine["dns1"]
-        dns2 = machine["dns2"]
-        dns_suffix = machine["dns_suffix"]
+        vcloud_config[name].each do |machine|
+          vdc_name = machine["vdc_name"]
+          edge_gateway = machine["edge_gateway"]
+          gateway = machine["gateway"]
+          netmask = machine["netmask"]
+          dns1 = machine["dns1"]
+          dns2 = machine["dns2"]
+          dns_suffix = machine["dns_suffix"]
 
-        start_address = machine["ip_ranges"][0]["start_address"]
-        end_address = machine["ip_ranges"][0]["end_address"]
+          start_address = machine["ip_ranges"][0]["start_address"]
+          end_address = machine["ip_ranges"][0]["end_address"]
 
-        puts ERB.new(File.read("./templates/vcd_networks.tf.erb")).result(binding)
+          puts ERB.new(File.read("./templates/vcd_networks.tf.erb")).result(binding)
+        end
       end
     end
   end
